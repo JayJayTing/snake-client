dc8a425feaf02a8721a564bc22013f11ecbabd76
JayJayTing <tingjayj@gmail.com>
2019-07-03 14:36:20 -0400

    done

--- play.js
+++ play.js
@@ -1,5 +1,21 @@
 const net = require('net');
 
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+}
+
+const handleUserInput = function(key){
+  if(key === "\u0003"){
+    process.exit();
+  }
+}
 /**
  * Establishes connection with the game server
  */
