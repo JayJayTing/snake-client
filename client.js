07bb9858f38f63d730395350187d76fb218cbda7
JayJayTing <tingjayj@gmail.com>
2019-07-03 14:04:01 -0400

    done

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require('net');
+const connect = function() {
+    const conn = net.createConnection({ 
+      host: 'localhost',
+      port: 50541
+    });
+    // interpret incoming data as text
+    conn.setEncoding('utf8'); 
+  
+    conn.on("connect", ()=> {
+      console.log("connected to server, hello from client");
+      conn.write("Name: JAY");
+    })
+    conn.on("data", (data)=>{
+      console.log(data);
+  
+    } )
+  
+  
+  
+    return conn;
+  }
+
+  module.exports = connect;
---
+++ play.js
@@ -0,0 +1,12 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+// play.js
+const connect = require('./client');
+console.log('Connecting ...');
+connect();
+
+
+
